54ead78dfece7772990e9e9d5871ebefebfdc2dc
syunya <syunya@syunyanoMacBook-puro-7.local>
2019-07-15 16:17:52 +0900

    中之島でテスト走行した

--- naka_motor_test/naka_motor_test.ino
+++ naka_motor_test/naka_motor_test.ino
@@ -22,9 +22,9 @@ double pos_x,pos_y,angle_offset,angle_deg,angle_rad;
 
 void messageCb(const geometry_msgs::Twist& twist) {
   const float linear_x = twist.linear.x;
-  const float angle_z = twist.angular.z;
-  rmo.writeMicroseconds(1500+100*(linear_x+angle_z));
-  lmo.writeMicroseconds(1500-100*(linear_x-angle_z));
+  const float angle_z = 0.7*twist.angular.z;
+  rmo.writeMicroseconds(1500+150*(linear_x+angle_z));
+  lmo.writeMicroseconds(1500-150*(linear_x-angle_z));
   if(twist.angular.x){
     pos_x=0;
     pos_y=0;
@@ -105,7 +105,7 @@ void loop() {
   double vel_x=diff_pos*cos(angle_rad)/dt;
   double vel_y=diff_pos*sin(angle_rad)/dt;
   double vel_z=(angle_rad-pre_rad)/dt;
-  angle_rad=pre_rad;
+  pre_rad=angle_rad;
 //tf
 
   t.header.frame_id = odom;
